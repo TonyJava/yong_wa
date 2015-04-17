729ced729db69f029694910805fb3d764ead49d5
羅中海 <luozhonghai1@luozhonghaideMacBook-Pro.local>
2015-04-17 22:33:31 +0900

    delete validate device_id

--- app/models/user_device.rb
+++ app/models/user_device.rb
@@ -2,5 +2,5 @@ class UserDevice < ActiveRecord::Base
   belongs_to :user
   belongs_to :device
   validates :user_id, presence: true
-  validates :device_id, presence: true
+  #validates :device_id, presence: true
 end
---
+++ server.js
@@ -0,0 +1,16 @@
+var redis = require("redis").createClient();
+redis.subscribe('user-created');
+var io = require('socket.io').listen(process.env.PORT || 5001);
+ io.on('connection', function(socket){
+ console.log('connected socket')
+ socket.on('disconnect', function(){
+ console.log('client disconnected')
+ socket.disconnect();
+ });
+});
+
+redis.on('message', function(channel, message){
+var info = JSON.parse(message);
+io.sockets.emit(channel, info);
+console.log('emit '+ channel);
+});
