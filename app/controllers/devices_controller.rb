fe06a7320f6e90d47d017bc20b12a7082131b740
羅中海 <luozhonghai1@luozhonghaideMacBook-Pro.local>
2015-08-17 12:09:49 +0900

    show device mobile on manager page

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -69,6 +69,6 @@ class DevicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def device_params
-      params.require(:device).permit(:series_code, :active, :config_info)
+      params.require(:device).permit(:series_code, :active, :config_info, :mobile)
     end
 end
